1bbc0addb635afaacf233c2214ea62d94bf9bc12
unknown <rickydon92@gmail.com>
2025-08-20 00:46:34 +0530

    Operators Program

---
+++ L-1/ArithmeticOperator.java
@@ -0,0 +1,36 @@
+public class ArithmeticOperator{
+
+
+
+public static void main(String[] args){
+
+int a=9;
+int b=8;
+
+System.out.println("value of a "+a);
+System.out.println("value of b "+b);
+int c= a+b;
+System.out.println("Addition of a and b => "+c);
+
+
+int d= a-b; // substraction 
+
+System.out.println("substraction of a and b => "+d);
+int m= a* b; // Multiplication
+System.out.println("multiply of a and b  => "+m);
+
+
+int div  = a/b;  // dividation of values
+
+System.out.println("Divide of a and b => "+div);
+
+int mod = a%b; // modulo
+
+System.out.println("Modulo of a and b => "+mod);
+
+
+
+	}
+
+
+}
